ef8d4412c4f4356369ac292ead36846e7c58c913
Fred Mitchell <fred.mitchell@gmx.de>
2017-04-29 19:20:19 +0200

    working out the generic syntax

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,14 @@
 #[cfg(test)]
-//#[macro_use(s)]
 
-//extern crate ndarray;
-//use ndarray::Array;
-//use ndarray::Dimension;
-//use ndarray::Axis;
+extern crate serde_json;
 
 mod gravity_set {
     
-    // COOR is a 2 or 3-element array of f64 defining
+    // T is a 2 or 3-element array of f64 defining
     // the coordinates of the star.
-    struct Star<COOR> {
+    struct Star<T> {
         mass: f64,
-        coordinate: COOR
+        coordinate: T
     }
     
     #[test]
@@ -20,17 +16,29 @@ mod gravity_set {
         let s: Star<[f64; 3]> = Star { mass: 100.0, coordinate: [1.1, 2.2, 3.3]};
     }
     
-    struct GSystem<COOR> {
-        //stars: Vec<Star>,
-        msize: u16,
-        lcorner: COOR,
-        ucorner: COOR
+    struct GSystem<T> {
+        stars: Vec<Star<T>>,
+        asize: u32,
+        lcorner: T,
+        ucorner: T
+    }
+
+
+    impl <T> GSystem<T> {
+        fn new(stars: Vec<Star<T>>,
+               msize: u16,
+               upper: T,
+               lower: T) -> GSystem<T> {
+            GSystem::<T> { stars: stars, asize: 2 ** msize, lcorner: lower, ucorner: upper }
+        }
     }
 
     #[test]
     fn gsystem_is_working() {
-        let gs: GSystem<[f64; 3]> = GSystem { msize: 8,
-                                              lcorner: [0.,0.,0.],
-                                              ucorner: [1.,1.,1.]};
+        let vs: vec![Star<[f64;3]>];
+        let gs: GSystem<[f64;3]> = GSystem::<[f64;3]>.new(stars: vs,
+                                                          msize: 8,
+                                                          lcorner: [0.,0.,0.],
+                                                          ucorner: [1.,1.,1.]);
     }
 }
