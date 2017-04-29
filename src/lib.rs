a65248d5d30cea46f2bf0a440c7bc4680bb8d708
Fred Mitchell <fred.mitchell@gmx.de>
2017-04-29 09:43:47 +0200

    some structures in place...

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,36 @@
 #[cfg(test)]
+//#[macro_use(s)]
+
+//extern crate ndarray;
+//use ndarray::Array;
+//use ndarray::Dimension;
+//use ndarray::Axis;
 
 mod gravity_set {
-}
-
-mod tests {
+    
+    // COOR is a 2 or 3-element array of f64 defining
+    // the coordinates of the star.
+    struct Star<COOR> {
+        mass: f64,
+        coordinate: COOR
+    }
+    
     #[test]
-    fn it_works() {
+    fn star_is_working() {
+        let s: Star<[f64; 3]> = Star { mass: 100.0, coordinate: [1.1, 2.2, 3.3]};
+    }
+    
+    struct GSystem<COOR> {
+        //stars: Vec<Star>,
+        msize: u16,
+        lcorner: COOR,
+        ucorner: COOR
+    }
+
+    #[test]
+    fn gsystem_is_working() {
+        let gs: GSystem<[f64; 3]> = GSystem { msize: 8,
+                                              lcorner: [0.,0.,0.],
+                                              ucorner: [1.,1.,1.]};
     }
 }
