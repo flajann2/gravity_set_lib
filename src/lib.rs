4819c41263d70336d95645513fb89153dee15044
Fred Mitchell <fred.mitchell@gmx.de>
2017-04-30 12:02:07 +0200

    the code compiles.

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,27 @@ mod gravity_set {
     
     // T is a 2 or 3-element array of f64 defining
     // the coordinates of the star.
-    struct Star<T> {
+    #[derive(Debug)]
+    pub struct Star<T> {
         mass: f64,
         coordinate: T
+    }
+
+    impl <T> Star<T> {
+        pub fn new(mass: f64, coord: T) -> Star<T> {
+            Star::<T> { mass: mass,
+                        coordinate: coord }
+        }
     }
     
     #[test]
     fn star_is_working() {
-        let s: Star<[f64; 3]> = Star { mass: 100.0, coordinate: [1.1, 2.2, 3.3]};
+        type A = [f64;3];
+        let s = Star::<A> { mass: 100.0, coordinate: [1.1, 2.2, 3.3]};
     }
     
-    struct GSystem<T> {
+    #[derive(Debug)]
+    pub struct GSystem<T> {
         stars: Vec<Star<T>>,
         asize: u32,
         lcorner: T,
@@ -25,20 +35,24 @@ mod gravity_set {
 
 
     impl <T> GSystem<T> {
-        fn new(stars: Vec<Star<T>>,
-               msize: u16,
-               upper: T,
-               lower: T) -> GSystem<T> {
-            GSystem::<T> { stars: stars, asize: 2 ** msize, lcorner: lower, ucorner: upper }
+        pub fn new(stars: Vec<Star<T>>,
+                   msize: u32,
+                   upper: T,
+                   lower: T) -> GSystem<T> {
+            GSystem::<T> { stars: stars,
+                           asize: 2u32.pow(msize),
+                           lcorner: lower,
+                           ucorner: upper }
         }
     }
 
     #[test]
     fn gsystem_is_working() {
-        let vs: vec![Star<[f64;3]>];
-        let gs: GSystem<[f64;3]> = GSystem::<[f64;3]>.new(stars: vs,
-                                                          msize: 8,
-                                                          lcorner: [0.,0.,0.],
-                                                          ucorner: [1.,1.,1.]);
+        type A = [f64;3];
+        type S = Star<A>;
+        let mut vs = Vec::<S>::new();
+        vs.push(S::new(1.0, [2.0, 3.1, -1.0]));
+        println!("vec: {:?}", vs);
+        let gs = GSystem::<A>::new(vs, 8, [0.,0.,0.], [1.,1.,1.]);
     }
 }
