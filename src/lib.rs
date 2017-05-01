c3f21b40f2935c8d98841edb64bed5249fee26cc
Fred Mitchell <fred.mitchell@gmx.de>
2017-05-01 20:40:06 +0200

    More functionality to be had here. using traits to allow for 2 or 3 dimensions... ndarray is back!

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,29 @@
 #[cfg(test)]
 
 extern crate serde_json;
+extern crate ndarray;
+
+use ndarray::Array;
+use ndarray::Array2;
+use ndarray::Array3;
+use ndarray::Dim;
 
 mod gravity_set {
+    /// 2- and 3-dimensional GS
+    pub trait Coord {}
+
+    impl Coord for [f64;2] {}
+    impl Coord for [f64;3] {}
     
-    // T is a 2 or 3-element array of f64 defining
-    // the coordinates of the star.
+    /// T is a 2 or 3-element array of f64 defining
+    /// the coordinates of the star.
     #[derive(Debug)]
-    pub struct Star<T> {
+    pub struct Star<T: Coord> {
         mass: f64,
         coordinate: T
     }
 
-    impl <T> Star<T> {
+    impl <T: Coord> Star<T> {
         pub fn new(mass: f64, coord: T) -> Star<T> {
             Star::<T> { mass: mass,
                         coordinate: coord }
@@ -24,28 +35,33 @@ mod gravity_set {
         type A = [f64;3];
         let s = Star::<A> { mass: 100.0, coordinate: [1.1, 2.2, 3.3]};
     }
-    
+
+    /// Total description of the
+    /// gravitational system.
     #[derive(Debug)]
-    pub struct GSystem<T> {
+    pub struct GSystem<T: Coord> {
         stars: Vec<Star<T>>,
         asize: u32,
+        delta: f64,
         lcorner: T,
         ucorner: T
     }
 
 
-    impl <T> GSystem<T> {
+    impl <T: Coord> GSystem<T> {
         pub fn new(stars: Vec<Star<T>>,
                    msize: u32,
+                   delta: f64,
                    upper: T,
                    lower: T) -> GSystem<T> {
             GSystem::<T> { stars: stars,
                            asize: 2u32.pow(msize),
+                           delta: delta,
                            lcorner: lower,
                            ucorner: upper }
         }
     }
-
+    
     #[test]
     fn gsystem_is_working() {
         type A = [f64;3];
@@ -53,6 +69,6 @@ mod gravity_set {
         let mut vs = Vec::<S>::new();
         vs.push(S::new(1.0, [2.0, 3.1, -1.0]));
         println!("vec: {:?}", vs);
-        let gs = GSystem::<A>::new(vs, 8, [0.,0.,0.], [1.,1.,1.]);
+        let gs = GSystem::<A>::new(vs, 8, 0.1, [0.,0.,0.], [1.,1.,1.]);
     }
 }
