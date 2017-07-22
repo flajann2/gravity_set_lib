ba964a5b2b0289c971d9e04bd2253d153ad64569
Fred Mitchell <fred.mitchell@gmx.de>
2017-07-22 14:35:22 +0200

    small changes

--- src/compute.rs
+++ src/compute.rs
@@ -3,6 +3,7 @@
 use ::{GSystem, Coord};
 use std::vec::Vec;
 use std::num;
+use std::ops::{Index, IndexMut};
 
 pub struct Field {
     plex: u32,
@@ -16,12 +17,27 @@ impl Field {
             space: vec![0; 2usize.pow(plex).pow(3u32)]
         }
     }
+
+    ///
+    pub fn ijk(&self, i: u32, j: u32, k: u32) -> &u16 {
+
+    }
+}
+
+impl Index<Coord> for Field {
+    type Output = u32;
+    fn index(&self, coord: Coord) -> u16 {
+        self.space[i]
+    }
 }
 
 #[test]
 fn field_is_working() {
     let plex = 8u32;
-    let vsize = 2usize.pow(plex).pow(3u32);
-    let f = Field::new(plex);
+    let side = 2usize.pow(plex);
+    let vsize = side.pow(3u32);
+    let mut f = Field::new(plex);
+
     assert_eq!(f.space.len(), vsize);
+    f.space[1] = 3;
 }
